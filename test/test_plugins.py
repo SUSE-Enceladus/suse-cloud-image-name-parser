e9a80987f7d44f5f716590817200f8153dbb2a23
Andres Pozo <andres.munoz@suse.com>
2025-12-12 18:21:50 +0100

    Removes unwanted headers

--- test/test_plugins.py
+++ test/test_plugins.py
@@ -1,21 +1,3 @@
-# Copyright (c) 2025 SUSE LLC
-#
-# This file is part of suse-cloud-image-name-parser
-#
-# image-name is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or # (at your option) any later version.
-#
-# image-name is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with image-name.
-# If not, see <http://www.gnu.org/licenses/>.
-
 import unittest
 
 from suse_cloud_image_name_parser.suse_cloud_image_name import (
